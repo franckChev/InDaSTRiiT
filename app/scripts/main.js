536ff49c872fb62d97887452fc0131a3de0594c2
Piranha <franck.chevallier@juloa.com>
2015-11-24 15:42:09 +0100

    open street map & chris

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,24 +2,33 @@
 /*global L */
 
 (function (window, document, L, undefined) {
-	'use strict';
+    'use strict';
 
-	L.Icon.Default.imagePath = 'images/';
+    L.Icon.Default.imagePath = 'images/';
 
-	/* create leaflet map */
-	var map = L.map('map', {
-		center: [52.5377, 13.3958],
-		zoom: 4
-	});
+    /* create leaflet map */
+    var map = L.map('map', {
+        center: [48.8139878, 2.3927642],
+        zoom: 17
+    });
 
-	/* add default stamen tile layer */
-	new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
-		minZoom: 0,
-		maxZoom: 18,
-		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
-	}).addTo(map);
+    var osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
+    new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib}).addTo(map);
 
-	L.marker([52.5, 13.4]).addTo(map);
+    var xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var geojson = L.geoJson(JSON.parse(xhr.responseText), {
+                    onEachFeature: function (feature, layer) {
+                    layer.bindPopup(feature.properties.name);
+                }
+            });
+            geojson.addTo(map);
+        }
+    }
+    xhr.open("GET", "data/profiles.json");
+    xhr.send();
 
 }(window, document, L));
